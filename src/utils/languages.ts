8787f485f761513525945772358d6d5cecc476c1
Trevor Kulczycki-McIntyre <123998731+Trevor-Km@users.noreply.github.com>
2025-06-02 22:25:42 -0400

    added new items to tech stack

--- src/utils/languages.ts
+++ src/utils/languages.ts
@@ -5,25 +5,26 @@ export interface Language {
 }
 
 export const languages: Record<string, Language> = {
-  angular: {
-    name: "Angular",
-    iconName: "angular",
-  },
   astro: {
     name: "Astro",
     iconName: "astro",
   },
-  bootstrap: {
-    name: "Bootstrap",
-    iconName: "bootstrap",
-  },
-  cloudflare: {
-    name: "Cloudflare",
-    iconName: "cloudflare",
-  },
   html: {
     name: "HTML 5",
     iconName: "html",
+  },
+  java: {
+    name: "Java",
+    iconName: "java",
+    className: "!bg-[#f6ece1]",
+  },
+  cs:{
+    name: "C#",
+    iconName: "csharp"
+  },
+  cpp: {
+    name: "C++",
+    iconName: "cpp"
   },
   javascript: {
     name: "JavaScript",
@@ -38,37 +39,17 @@ export const languages: Record<string, Language> = {
     className: "!bg-[#f6ece1]",
     iconName: "mysql",
   },
-  wordpress: {
-    name: "Wordpress",
-    iconName: "wordpress",
-  },
   node: {
     name: "Node.js",
     iconName: "node",
-  },
-  tailwind: {
-    name: "Tailwind CSS",
-    iconName: "tailwind",
-  },
-  figma: {
-    name: "Figma",
-    iconName: "figma",
   },
   firebase: {
     name: "Firebase",
     iconName: "firebase",
   },
-  markdown: {
-    name: "Markdown",
-    iconName: "markdown",
-  },
   php: {
     name: "PHP",
     iconName: "php",
-  },
-  sass: {
-    name: "Sass",
-    iconName: "sass",
   },
   ts: {
     name: "TypeScript",
@@ -78,33 +59,9 @@ export const languages: Record<string, Language> = {
     name: "Git",
     iconName: "git",
   },
-  css: {
-    name: "CSS",
-    iconName: "css",
-  },
-  vercel: {
-    name: "Vercel",
-    iconName: "vercel",
-  },
-  netlify: {
-    name: "Netlify",
-    iconName: "netlify",
-  },
-  gatsby: {
-    name: "Gatsby",
-    iconName: "gatsby",
-  },
-  windsurf: {
-    name: "Windsurf",
-    iconName: "windsurf-logo",
-  },
   cursor: {
     name: "Cursor",
     iconName: "cursor-ia",
-  },
-  deepseek: {
-    name: "DeepSeek",
-    iconName: "deepseek",
   },
   python: {
     name: "Python",
